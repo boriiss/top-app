599d8183361feccaa78eb6de877985e8617e4148
Boris <boriswork1996@gmail.com>
2024-01-12 02:31:10 +0300

    8.8. Компонент рейтинга - 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
+"use client"
+import { useState } from 'react';
 import { Metadata } from 'next';
 import { Button, Htag, P, Rating, Tag } from '../components';
-
-export const metadata: Metadata = {
-  title: 'Исправленный заголок',
-};
 
 export async function GenerateMetadata(): Promise<Metadata> {
   return {
@@ -13,6 +11,8 @@ export async function GenerateMetadata(): Promise<Metadata> {
 
 
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <>
       <Htag tag='h2'>Hello</Htag>
@@ -25,7 +25,7 @@ export default function Home(): JSX.Element {
       <Tag size='m' color='red'>Red</Tag>
       <Tag size='s' color='green'>Green</Tag>
       <Tag color='primary'>Primary</Tag>
-      <Rating rating={4} />
+      <Rating rating={rating} isEditable setRating={setRating} />
     </>
   );
 }
